50d82d96fa2be3232462a3a4292a55e9c74e3280
Akash Manohar J <akash@akash.im>
2014-02-20 13:30:14 +0530

    Basic grid layout with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 GetMogoChat::Application.routes.draw do
 
   root "main#index"
+  get "landing" => "main#landing"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
